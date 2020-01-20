36d12cf6e0f65de49fa208974d7a9ad55ce1ac6e
Avkash Hirpara <avkash.av1987@gmail.com>
2020-01-20 20:32:01 +0530

    Adding Index page and controller. closes #8.

--- pet-clinic-data/src/main/java/mycraft/springframwork/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mycraft/springframwork/sfgpetclinic/services/VetService.java
@@ -2,7 +2,5 @@ package mycraft.springframwork.sfgpetclinic.services;
 
 import mycraft.springframwork.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet,Long>{
 }
