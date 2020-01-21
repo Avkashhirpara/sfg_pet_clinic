310972c9a8d4d382374bcb08dc2c243e71444c5f
Avkash Hirpara <avkash.av1987@gmail.com>
2020-01-21 23:55:39 +0530

    adding Owners list and updated controler. closes #16.

--- pet-clinic-web/src/main/java/mycraft/springframwork/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/mycraft/springframwork/sfgpetclinic/controller/OwnerController.java
@@ -1,14 +1,25 @@
 package mycraft.springframwork.sfgpetclinic.controller;
 
+import mycraft.springframwork.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+    private final OwnerService ownerService;
 
-@RequestMapping({"/","","/index","index.html"})
-public String listOwners(){
-    return "owners/index";
-}
-}
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"/","","/index","index.html"})
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
+        return "owners/index";
+    }
+
+
+
+    }
