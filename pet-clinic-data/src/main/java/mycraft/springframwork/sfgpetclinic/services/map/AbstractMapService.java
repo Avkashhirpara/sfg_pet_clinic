8cf29c48f30dd00318bdad17900a782978609c21
Avkash Hirpara <avkash.av1987@gmail.com>
2020-01-22 00:20:31 +0530

    Refactored Map impl to abstract Id generation. closes #23

--- pet-clinic-data/src/main/java/mycraft/springframwork/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/mycraft/springframwork/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,27 +1,47 @@
 package mycraft.springframwork.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import mycraft.springframwork.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
 
-    Set<T> findAll(){
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
+
+    Set<T> findAll() {
         return new HashSet<>(map.values());
     }
-    T findById(ID id){
+
+    T findById(ID id) {
         return map.get(id);
     }
-    T save (ID id,T object){
-        map.put(id,object);
+
+    T save(T object) {
+        if(object !=null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+        map.put(object.getId(), object);
+        }else{
+            throw new RuntimeException("object is null");
+        }
         return object;
     }
-    void deleteById(ID id){
+
+    void deleteById(ID id) {
         map.remove(id);
     }
-    void delete(T object){
-        map.entrySet().removeIf(entry->entry.getValue().equals(object));
+
+    void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
+    private Long getNextId(){
+        Long nextid  = null;
+        try {
+            nextid = Collections.max(map.keySet()) + 1;
+        }catch (NoSuchElementException e){
+            nextid = 1L;
+        }
+        return nextid;
+    }
+
 }
