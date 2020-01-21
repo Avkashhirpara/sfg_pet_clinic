ce341fabf3fb926e05431f97e60b1080530794c0
Avkash Hirpara <avkash.av1987@gmail.com>
2020-01-22 00:04:56 +0530

    Adding vet list and updated controler. closes #15

--- pet-clinic-web/src/main/java/mycraft/springframwork/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mycraft/springframwork/sfgpetclinic/bootstrap/DataLoader.java
@@ -32,7 +32,7 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2l);
+        owner2.setId(2l);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
@@ -47,7 +47,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2l);
+        vet2.setId(2l);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
--- pet-clinic-web/src/main/java/mycraft/springframwork/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/mycraft/springframwork/sfgpetclinic/controller/VetController.java
@@ -1,12 +1,21 @@
 package mycraft.springframwork.sfgpetclinic.controller;
 
+import mycraft.springframwork.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets/index","/vets","/vets/index.html"})
-    public String listVet(){
+    public String listVet(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
