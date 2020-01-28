97fb9aa776c55b25a7cee7bd4f04d76816f539da
Avkash Hirpara <avkash.av1987@gmail.com>
2020-01-28 21:24:38 +0530

    update vet with jpa entity mapping. Closes #37

--- pet-clinic-data/src/main/java/mycraft/springframwork/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/mycraft/springframwork/sfgpetclinic/model/Vet.java
@@ -1,9 +1,15 @@
 package mycraft.springframwork.sfgpetclinic.model;
-
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name ="vets")
 public class Vet extends Person {
+
+    @ManyToMany(fetch =  FetchType.EAGER)
+    @JoinTable(name="vet_specialities", joinColumns =  @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name="specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
