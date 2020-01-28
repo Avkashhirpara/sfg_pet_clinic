715dd91db992751611acc22a635bb3b5a26f65b6
Avkash Hirpara <avkash.av1987@gmail.com>
2020-01-28 21:02:55 +0530

    Updated base entity with JPA Annotation. Closes #34.

--- pet-clinic-data/src/main/java/mycraft/springframwork/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/mycraft/springframwork/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,14 @@
 package mycraft.springframwork.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -12,4 +18,5 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
 }
